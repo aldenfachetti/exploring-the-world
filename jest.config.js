f7f50ce78161ff960cc6996a56089218d3fe6277
aldenfachetti <aldenmerlin@gmail.com>
2022-12-06 09:57:40 +0100

    Making the Map more dynamics

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.tsx'],
+  modulePaths: ['<rootDir>/src/']
+  // transformIgnorePatterns: [
+  //   './node_modules/(?!(react-leaflet/lib|@react-leaflet/core/lib)/)'
+  // ]
 }
